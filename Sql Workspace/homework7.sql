cfaaf32ae1d1c15284ead3d736aad94ef336a244
enessertaslan <45246609+enessertaslan@users.noreply.github.com>
2022-02-28 20:54:33 +0300

    Update homework7.sql

--- Sql Workspace/homework7.sql
+++ Sql Workspace/homework7.sql
@@ -8,3 +8,15 @@ GROUP BY rating;
 SELECT replacement_cost,COUNT(*) FROM film
 GROUP BY replacement_cost
 HAVING COUNT(replacement_cost)>50;
+
+3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id;
+
+4.city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+
+SELECT country_id ,COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
